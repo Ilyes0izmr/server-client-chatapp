146297e5b33c9ac49d29e85053bb6177a8321087
hama-Key <midomeftah05@gmail.com>
2025-12-11 22:28:14 +0100

    task 5 done (pre-final-testing using 2 PCs) : Add encryption for exchanged messages using SSL module

--- server/ui/main_window.py
+++ server/ui/main_window.py
@@ -7,7 +7,7 @@ from server.ui.thread_signals import ThreadSignals
 from server.core.tcp_server import TCPServer
 from server.core.udp_server import UDPServer
 
-host_value = '10.137.154.179'
+host_value = 'localhost'
 class ServerMainWindow(QMainWindow):
     """Main server window with complete chat functionality"""
     
